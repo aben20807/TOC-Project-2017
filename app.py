86e3224ae640da51d4ae3ac84fb167a25792e906
aben20807 <aben20807@gmail.com>
2017-12-31 16:54:43 +0800

    Can read url and token from file, ignore sensitive data

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+import os
 import sys
 from io import BytesIO
 
@@ -6,44 +8,57 @@ from flask import Flask, request, send_file
 
 from fsm import TocMachine
 
+os.system("curl  http://localhost:4040/api/tunnels > tunnels.json")
+data_file =  open('tunnels.json', 'r')
+try:
+    datajson = json.load(data_file)
+    for i in datajson['tunnels']:
+        WEBHOOK_URL = i['public_url']
+finally:
+    data_file.close()
 
-API_TOKEN = 'Your Telegram API Token'
-WEBHOOK_URL = 'Your Webhook URL'
+token_file =  open('api_token.json', 'r')
+try:
+    datajson = json.load(token_file)
+    for i in datajson['api_token']:
+        API_TOKEN = i['token']
+finally:
+    token_file.close()
 
 app = Flask(__name__)
 bot = telegram.Bot(token=API_TOKEN)
 machine = TocMachine(
-    states=[
-        'user',
-        'state1',
-        'state2'
-    ],
-    transitions=[
-        {
-            'trigger': 'advance',
-            'source': 'user',
-            'dest': 'state1',
-            'conditions': 'is_going_to_state1'
-        },
-        {
-            'trigger': 'advance',
-            'source': 'user',
-            'dest': 'state2',
-            'conditions': 'is_going_to_state2'
-        },
-        {
-            'trigger': 'go_back',
-            'source': [
-                'state1',
-                'state2'
+        states=[
+            'user',
+            'state1',
+            'state2'
             ],
-            'dest': 'user'
-        }
-    ],
-    initial='user',
-    auto_transitions=False,
-    show_conditions=True,
-)
+        transitions=[
+            {
+                'trigger': 'advance',
+                'source': 'user',
+                'dest': 'state1',
+                'conditions': 'is_going_to_state1'
+                },
+            {
+                'trigger': 'advance',
+                'source': 'user',
+                'dest': 'state2',
+                'conditions': 'is_going_to_state2'
+                },
+            {
+                'trigger': 'go_back',
+                'source': [
+                    'state1',
+                    'state2'
+                    ],
+                'dest': 'user'
+                }
+            ],
+        initial='user',
+        auto_transitions=False,
+        show_conditions=True,
+        )
 
 
 def _set_webhook():
